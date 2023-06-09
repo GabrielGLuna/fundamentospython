754a5a2ce5d7f2dbe3f6892b8467369285db1007
GabrielGarcia <gabrielunagarcia@hotmail.com>
2023-06-09 16:52:26 -0600

    04 operadores

---
+++ operadores.py
@@ -0,0 +1,15 @@
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+
+print(2 ** 6)
+print(10 / 3)
+print(10 // 3)
+
+#Operadores
+# + para concatenar
+# * para replicar
+print("--------" * 4)
+print("Hola " +  " Mundo!")
+print("Hola " * 3)
