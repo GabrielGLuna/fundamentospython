56b575a2a16450a951cace14a05f5d6fc7bdd23d
GabrielGarcia <gabrielunagarcia@hotmail.com>
2023-06-09 16:43:15 -0600

    03 entrada de datos

---
+++ entrada.py
@@ -0,0 +1,19 @@
+num_a = int(input("Ingresa un numero : "))
+num_b = int(input("Ingresa otro numero : "))
+
+print(num_a + num_b)
+
+name = input("Ingresa tu nombre : ")
+age = int(input("Ingresa tu edad : "))
+city = input("Ingresa tu viudad/pueblo : ")
+
+#String Format
+""""
+Holaa, soy name, tengo age años y vivo en cit
+"""
+greeting ="Hola soy {}, tengo {} años y vivo en {}"
+
+print(greeting.format(name,age,city))
+
+greeting_2 =f"Holaa, soy {name},tengo {age} años y vivo en {city}"
+print(greeting_2)
---
+++ variables.py
@@ -0,0 +1,14 @@
+name = "Gabriel"
+last_name="Garcia"
+age=39
+height=1.68
+is_student=False
+groups=["3A","3B","3C"]
+
+#Funcion type
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
