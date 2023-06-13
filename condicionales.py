6fdafc2f549f3e7e3d21d862f31ed4efeacc3650
GabrielGarcia <gabrielunagarcia@hotmail.com>
2023-06-13 12:44:26 -0600

    06 condicionales

---
+++ condicionales.py
@@ -0,0 +1,17 @@
+age = 4
+
+if age>=18:
+    print("Es mayor de edad")
+
+    if age<18:
+        print("Es menor de edad")
+    else:
+        print("Es mayor de edad")
+
+age = 75
+if age<18:
+    print("Es menor de edad")
+elif age<70:
+    print ("Es mayor de edad")
+else:
+    print ("Es adulto mayor")
---
+++ piedra_papel.py
@@ -0,0 +1,45 @@
+#Elección aleatoria Maq
+import random
+
+#function randint(min,max)
+rand_int = random.randint(1,3)
+if rand_int ==1: 
+    choice_maq='Piedra'
+elif rand_int == 2: 
+    choice_maq='Papel'
+else:
+    choice_maq = 'Tijeras'
+#Elección de usuario
+
+choice_text = '''
+Escribe una de las opciones : 
+   Piedra 
+   Papel
+   Tijeras
+   '''
+
+choice_user = input(choice_text)
+#Impresión de opciones
+print('Usuario eligió ->', choice_user)
+print('Maquina eligió ->', choice_maq)
+
+#Ganador!!!!!!!!!!
+if choice_maq==choice_user:
+    print("  Es un empate!!  ")
+
+else:
+    if choice_maq=='Piedra' and choice_user=='Papel':
+        print("  Gana Usuario!!  ")
+    elif choice_maq == 'Piedra' and choice_user=='Tijeras':
+        print("  Gana Maquina!!  ")
+    elif choice_maq=='Papel' and choice_user=='Tijeras':
+        print("  Gana el Usuario!!  ")
+    elif choice_maq=='Papel' and choice_user=='Piedra':
+        print("  Gana la Maquina!!  ")
+    elif choice_maq=='Tijeras' and choice_user=='Piedra':
+        print("  Gana el Usuario!!  ")
+    else:
+        print("  Gana la maquina!!  ")
+
+
+
