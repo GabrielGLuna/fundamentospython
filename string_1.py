76493355385c9492474c1e46c00a5572e98df697
GabrielGarcia <gabrielunagarcia@hotmail.com>
2023-06-13 13:03:02 -0600

    07 Strings

---
+++ string_1.py
@@ -0,0 +1,25 @@
+
+my_string = 'esTe eS mI StrinG'
+
+print(len(my_string))
+
+print(my_string.upper())
+
+print(my_string.lower())
+
+print(my_string.capitalize())
+
+print(my_string.strip())
+
+my_string = "Hola 3C DSM"
+
+
+print(my_string[0])
+print(my_string[3])
+print(my_string[4])
+print(my_string[3])
+print(my_string[4])
+print(my_string[1])
+
+
+print(my_string[0:2])
